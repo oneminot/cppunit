4dbb34e22f9c04da1163d7ce2195ba8f27b508fb
kushal <kushaldeveloper@gmail.com>
2014-11-06 11:50:08 -0600

    fails

---
+++ RoaringRalph.Tests/mainTests.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RoaringRalphTests
+{
+	TEST_CLASS(mainTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
